7b9e2c3ea950220758946736c13af73c927cdb0e
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-11-09 01:30:26 +0900

    Create Kinesys RSA p, q값이 비슷할 경우 n 값으로 p, q값 구하기.py

---
+++ p, q값이 비슷할 경우 n 값으로 p, q값 구하기/Kinesys RSA p, q값이 비슷할 경우 n 값으로 p, q값 구하기.py
@@ -0,0 +1,25 @@
+#Kinesys RSA p, q값이 비슷할 경우 n 값으로 p, q값 구하기.py
+##n, e, c 값이 주어졌다고 가정
+
+from gmpy2 import *
+
+n =
+
+e =
+
+c =
+
+p = isqrt(n)
+
+while True:
+    q, r = t_divmod(n, p)
+    
+    if r == 0:
+        break
+    p += 1
+
+phi = (p-1) * (q-1)
+
+d = invert(e, phi)
+
+print ('%x' %pow(c,d,n)).decode("hex")
