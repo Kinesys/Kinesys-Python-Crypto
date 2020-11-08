efad047888d7ce1a2980790a34639478407ac734
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-11-09 01:30:53 +0900

    Create Kinesys RSA d값 구하기.py

---
+++ d값 구하기/Kinesys RSA d값 구하기.py
@@ -0,0 +1,16 @@
+#Kinesys RSA d값 구하기.py
+#p, q, c, e 값이 주어졌을 경우
+import gmpy2
+
+p = 
+q =
+e =
+c =
+
+n = p * q
+
+phi = (p-1) * (q-1)
+
+d = invert(e, phi)
+
+print('%x' %pow(c, d, n)).decode("hex")
