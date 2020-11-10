059a75021fd90a22951dfdbb88dcaae78d93b4d1
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-11-10 10:38:10 +0900

    Create Kinesys AES decrypt Example(key, cipher 주어진 경우).py

---
+++ AES/Kinesys AES decrypt Example(key, cipher 주어진 경우).py
@@ -0,0 +1,17 @@
+#Kinesys AES decrypt Example(key, cipher 주어진 경우).py
+
+from Crypto.Cipher import AES
+
+cipher = 'f9a385a21faa40ae6313d225987b87c554ca0a2e32a42a28a71c1c155ec787d76a4ea9aeb71339335747b91bd50faa99'
+
+key = 7901925
+key = str(key)
+
+while len(key) < 32:
+    key = '0' + key
+
+c = AES.new(key, AES.MODE_ECB)
+
+result = c.decrypt(cipher)
+
+print result
